13782a6f428df8dd0afa6256e7155a04484a0694
RobertoGD <contact@robertogd.com>
2017-04-01 18:04:30 +0100

    Models and migration changes

--- shortener/models.py
+++ shortener/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class KirrURL(models.Model):
     url = models.CharField(max_length=220, )
     shortcode = models.CharField(max_length=15, unique=True)
+    updated = models.DateTimeField(auto_now=True) #everytime the model is saved
+    timestamp = models.DateTimeField(auto_now_add=True) #when model was created
+    #empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     #shortcode = models.CharField(max_length=15, null=True) Empty in database is ok
     #shortcode = models.CharField(max_length=15, default='kirrdefaultshortcode')
 
